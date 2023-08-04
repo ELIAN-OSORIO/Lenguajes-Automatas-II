3820bab951d6f0e6d130a51ac0bd8b874e479403
ELIAN-OSORIO <al19760601@ite.edu.mx>
2023-08-03 21:30:01 -0700

    Project-Scanner

---
+++ Scanner/Scanner/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Scanner.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Scanner
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new UserControl1();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DataContext = new UserControl2();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            DataContext = new UserControl3();
+        }
+    }
+}
