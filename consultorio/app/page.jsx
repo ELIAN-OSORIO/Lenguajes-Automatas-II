2caa9458a9d87d829725e82058a78f85fdaa7523
San Miguel <c-jsanmiguel@allegion.com>
2023-09-20 12:26:10 -0700

    Tarea-Xenia

---
+++ consultorio/app/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <main>Este es el contenido Principal</main>
+
+      <footer className='pie-pagina'>
+        <div className='grupo-1'>
+            <div className="box">
+              <h2>ACERCA DE</h2>
+              <h2>Esta es una pequena prueba sobre mi footer en react native</h2>
+            </div>
+            <div className="box">
+              <h2>UBICACION</h2>
+              <h2>Instituto Tecnologico de Ensenada</h2>
+            </div>
+            <div className="box">
+              <h2>CONTACTO</h2>
+              <h2>al19760601@ite.edu.mx</h2>
+              <h2>646 288 99 98</h2>
+            </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
